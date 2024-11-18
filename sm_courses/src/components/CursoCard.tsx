f3d1a846edadd9c2d58fcb3b7f06cba798cd6910
koalaDaProgramação <110944337+Getsuga01@users.noreply.github.com>
2024-11-18 12:11:08 -0300

    Update CursoCard.tsx

--- sm_courses/src/components/CursoCard.tsx
+++ sm_courses/src/components/CursoCard.tsx
@@ -2,13 +2,13 @@ interface CursoCardProps {
     nome: string;
     descricao: string;
     professor: string;
-    onEdit?: () => void;
-    onDelete?: () => void;
-    onView?: () => void; // Nova funcionalidade opcional
+    onEdit?: () => void; // Função opcional para editar o curso
+    onDelete?: () => void; // Função opcional para excluir o curso
+    onView?: () => void; // Função opcional para visualizar detalhes do curso
   }
-  
+  // Componente funcional que renderiza um cartão de curso
   export function CursoCard({ nome, descricao, professor, onEdit, onDelete, onView }: CursoCardProps) {
-    return (
+    return ( // renderiza um cartão de curso
       <div
         onClick={onView}
         className="flex flex-col gap-4 pb-4 w-1/4 border border-slate-950 rounded shadow-2xl hover:animate-pulse cursor-pointer"
